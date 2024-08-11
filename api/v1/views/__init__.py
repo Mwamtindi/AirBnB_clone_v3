449fb9b527b4a21aa7387e96e1c202552f062328
Mwamtindi <mwamtindishaban@gmail.com>
2024-08-11 22:44:55 +0300

    update init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-The views
+The views module for the API.
+Module initializes Blueprint for the API views and imports all view modules.
 """
-
 from flask import Blueprint
 
-app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
+app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
 from api.v1.views.states import *
